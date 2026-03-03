4fd93d4e36b68ea424ac1e010892c562970b4a33
Tanishq8806 <ts8806@srmist.edu.in>
2026-03-03 14:59:27 +0530

    UC2 - Print a Hardcoded Palindrome Result

--- main/src/PalindromeChecker.java
+++ main/src/PalindromeChecker.java
@@ -1,7 +1,18 @@
-public class PalindromeChecker{
+import java.util. Scanner;
+public class PalindromeChecker {
     public static void main(String[] args){
-    System.out.println("Welcome to the palindrome checker management system");
-    System.out.println("Version: 1.0");
-    System.out.println("System Initialised Sucessfully");
+        String word="madam";
+        boolean ispalindrome=true;
+        for(int i=0;i<word.length()/2;i++){
+          if(word.charAt(i) != word.charAt(word.length()-1-i)){
+            ispalindrome = false;
+            break;
+          }
+        }
+        if (ispalindrome){
+            System.out.println(word+ " is a palindrome");
+        }else{
+            System.out.println(word+" is not a palindrone");
+        }
     }
 }
